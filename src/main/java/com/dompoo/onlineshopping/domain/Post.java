394341c2e38f4f09cf625d193069285959b0efef
Dompoo <dlckdrms0517@gmail.com>
2023-09-21 19:55:23 +0900

    fix : Entity에 getter추가

--- src/main/java/com/dompoo/onlineshopping/domain/Post.java
+++ src/main/java/com/dompoo/onlineshopping/domain/Post.java
@@ -2,9 +2,11 @@ package com.dompoo.onlineshopping.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
 
