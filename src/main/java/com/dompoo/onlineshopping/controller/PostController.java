c44c1d10d57cea301502d8213ecfae2afdb60122
Dompoo <dlckdrms0517@gmail.com>
2023-09-21 19:34:02 +0900

    feat : post 컨트롤러 생성

---
+++ src/main/java/com/dompoo/onlineshopping/controller/PostController.java
@@ -0,0 +1,27 @@
+package com.dompoo.onlineshopping.controller;
+
+import com.dompoo.onlineshopping.request.PostCreateRequest;
+import com.dompoo.onlineshopping.service.PostService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping("/posts")
+    public Map<String, String> post(@RequestBody @Valid PostCreateRequest request) {
+
+        postService.write(request);
+        return Map.of();
+    }
+}
