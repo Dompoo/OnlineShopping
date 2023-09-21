a719ed4bbac040c84f75edcbe6cc6a8c2a9494cc
Dompoo <dlckdrms0517@gmail.com>
2023-09-21 19:34:25 +0900

    feat : post 서비스, 레포지토리 생성

---
+++ src/main/java/com/dompoo/onlineshopping/service/PostService.java
@@ -0,0 +1,21 @@
+package com.dompoo.onlineshopping.service;
+
+import com.dompoo.onlineshopping.domain.Post;
+import com.dompoo.onlineshopping.repository.PostRepository;
+import com.dompoo.onlineshopping.request.PostCreateRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public void write(PostCreateRequest postCreateRequest) {
+
+        postRepository.save(new Post(postCreateRequest.getTitle(), postCreateRequest.getContent()));
+    }
+}
