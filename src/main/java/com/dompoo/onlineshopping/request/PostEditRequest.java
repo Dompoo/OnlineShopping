0b52434c89de228cbde1ef48cab8c696093e4473
Dompoo <dlckdrms0517@gmail.com>
2023-09-24 12:43:16 +0900

    feat : 글 수정을 위한 빌더 클래스 작성

---
+++ src/main/java/com/dompoo/onlineshopping/request/PostEditRequest.java
@@ -0,0 +1,21 @@
+package com.dompoo.onlineshopping.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class PostEditRequest {
+
+    private String title;
+    private String content;
+
+    @Builder
+    public PostEditRequest(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
