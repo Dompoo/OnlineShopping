30e1d57fbc57fe9a6d06b976078721fdc00df835
Dompoo <dlckdrms0517@gmail.com>
2023-09-28 16:31:53 +0900

    feat : Q파일 관리시작

---
+++ src/main/generated/com/dompoo/onlineshopping/domain/QProduct.java
@@ -0,0 +1,41 @@
+package com.dompoo.onlineshopping.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QProduct is a Querydsl query type for Product
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QProduct extends EntityPathBase<Product> {
+
+    private static final long serialVersionUID = -609378459L;
+
+    public static final QProduct product = new QProduct("product");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final NumberPath<Integer> price = createNumber("price", Integer.class);
+
+    public final StringPath productName = createString("productName");
+
+    public QProduct(String variable) {
+        super(Product.class, forVariable(variable));
+    }
+
+    public QProduct(Path<? extends Product> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QProduct(PathMetadata metadata) {
+        super(Product.class, metadata);
+    }
+
+}
+
