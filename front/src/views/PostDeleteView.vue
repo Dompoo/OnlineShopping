13f6aa40abc12a1b23447bf4a71f545c4a805771
Dompoo <dlckdrms0517@gmail.com>
2023-09-29 01:11:05 +0900

    feat : 글 삭제 뷰 작성

---
+++ front/src/views/PostDeleteView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+import {onMounted} from "vue";
+import axios from "axios";
+import {useRouter} from "vue-router";
+
+const router = useRouter();
+
+
+const props = defineProps({
+  postId: {
+    type: [Number, String],
+    require: true,
+  },
+});
+
+onMounted(() => {
+  axios.delete(`/onlineShopping-api/posts/${props.postId}`)
+      .then(() => {
+        router.replace({name: "home"});
+      });
+})
+</script>
