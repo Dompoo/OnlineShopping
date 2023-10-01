50db3b3878ab3b9d2a104edac6ae5edb36245720
Dompoo <dlckdrms0517@gmail.com>
2023-10-01 11:59:50 +0900

    feat : 쿠키를 통한 토큰 전달

--- src/main/java/com/dompoo/onlineshopping/config/AuthResolver.java
+++ src/main/java/com/dompoo/onlineshopping/config/AuthResolver.java
@@ -4,13 +4,17 @@ import com.dompoo.onlineshopping.config.data.UserSession;
 import com.dompoo.onlineshopping.domain.Session;
 import com.dompoo.onlineshopping.exception.Unauthorized;
 import com.dompoo.onlineshopping.repository.SessionRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Slf4j
 @RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
 
@@ -23,10 +27,19 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        String accessToken = webRequest.getHeader("Authorization");
-        if (accessToken == null || accessToken.isEmpty()) {
+        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+        if (servletRequest == null) {
+            log.error("servletRequest is NULL!");
             throw new Unauthorized();
         }
+        Cookie[] cookies = servletRequest.getCookies();
+
+        if (cookies == null || cookies.length == 0) {
+            log.error("NO COOKIES!");
+            throw new Unauthorized();
+        }
+
+        String accessToken = cookies[0].getValue();
 
         Session session = sessionRepository.findByAccessToken(accessToken)
                 .orElseThrow(Unauthorized::new);
--- src/main/java/com/dompoo/onlineshopping/controller/AuthController.java
+++ src/main/java/com/dompoo/onlineshopping/controller/AuthController.java
@@ -2,14 +2,18 @@ package com.dompoo.onlineshopping.controller;
 
 import com.dompoo.onlineshopping.config.data.UserSession;
 import com.dompoo.onlineshopping.request.LoginRequest;
-import com.dompoo.onlineshopping.response.SessionResponse;
 import com.dompoo.onlineshopping.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 @Slf4j
 @RestController
@@ -25,8 +29,24 @@ public class AuthController {
     }
 
     @PostMapping("/auth/login")
-    public SessionResponse login(@RequestBody LoginRequest request) {
+    public ResponseEntity<Object> login(@RequestBody LoginRequest request) {
         String accessToken = authService.signin(request);
-        return new SessionResponse(accessToken);
+
+        ResponseCookie cookie = ResponseCookie
+                .from("SESSION", accessToken)
+                .domain("localhost") //TODO 서버 환경에 따른 분리 필요
+                .path("/") //특정 서블릿에만 쿠키를 전달할 것인가?
+                .httpOnly(true) //자바스크립트를 통한 쿠키탈취를 방어할 것인가?
+                .secure(false) //https 요청에만 쿠키를 전달할 것인가?
+                .maxAge(Duration.ofDays(30))
+                .sameSite("Strict") //서드파티요청에 쿠키를 전달할 것인가?
+                .build();
+
+        log.info(">> cookie = {}", cookie);
+
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .build();
     }
 }
