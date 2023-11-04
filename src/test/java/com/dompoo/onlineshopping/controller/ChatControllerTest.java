d15997fdd8878791829c810966fe16299089915b
Dompoo <dlckdrms0517@gmail.com>
2023-11-04 20:01:08 +0900

    test : 챗 리스트 가져오기 테스트 작성

--- src/test/java/com/dompoo/onlineshopping/controller/ChatControllerTest.java
+++ src/test/java/com/dompoo/onlineshopping/controller/ChatControllerTest.java
@@ -1,10 +1,7 @@
 package com.dompoo.onlineshopping.controller;
 
 import com.dompoo.onlineshopping.TestUtil;
-import com.dompoo.onlineshopping.domain.Conversation;
-import com.dompoo.onlineshopping.domain.Post;
-import com.dompoo.onlineshopping.domain.Product;
-import com.dompoo.onlineshopping.domain.User;
+import com.dompoo.onlineshopping.domain.*;
 import com.dompoo.onlineshopping.repository.ChatRepository;
 import com.dompoo.onlineshopping.repository.ConversationRepository;
 import com.dompoo.onlineshopping.repository.UserRepository;
@@ -21,9 +18,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
@@ -104,6 +104,48 @@ class ChatControllerTest {
                 )
                 .andExpect(status().isOk())
                 .andDo(print());
+    }
+
+    @Test
+    @DisplayName("채팅 조회하기")
+    void getChatList() throws Exception {
+        //given
+        User addUser = userRepository.save(testUtil.newUserBuilderPlain()
+                .build());
+
+        Product savedProduct = productRepository.save(testUtil.newProductBuilder()
+                .user(addUser)
+                .build());
+
+        Post savedPost = postRepository.save(testUtil.newPostBuilder()
+                .user(addUser)
+                .product(savedProduct)
+                .build());
+
+        Conversation savedConv = conversationRepository.save(Conversation.builder()
+                .post(savedPost)
+                .build());
+
+        Chat savedChat1 = chatRepository.save(Chat.builder()
+                .message("첫번째 채팅!")
+                .conversation(savedConv)
+                .build());
+
+        Chat savedChat2 = chatRepository.save(Chat.builder()
+                .message("두번째 채팅!")
+                .conversation(savedConv)
+                .build());
+
+        //when
+        mockMvc.perform(get("/posts/{convId}/chat", savedConv.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()", is(2)))
+                .andExpect(jsonPath("$[0].message").value("첫번째 채팅!"))
+                .andExpect(jsonPath("$[1].message").value("두번째 채팅!"))
+                .andDo(print());
+
+        //then
+
     }
 
 }
